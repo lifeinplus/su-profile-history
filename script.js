6968e22dd56b7db3ce8ad7ee00aca720191506ac
Artem Denisov <lifeinplus@gmail.com>
2024-07-20 03:24:35 +0200

    DEV-105 Profile and History Page

---
+++ script.js
@@ -0,0 +1,9 @@
+function showProfile() {
+    document.getElementById("profile").style.display = "block";
+    document.getElementById("history").style.display = "none";
+}
+
+function showHistory() {
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("history").style.display = "block";
+}
